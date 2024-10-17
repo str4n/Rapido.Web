bd2eee48641d4ebd1884c42aba8ffbb3de1475d7
Kamil <Str4ngerGameOne@gmail.com>
2024-10-17 16:36:42 +0200

    Landing page and navbar

--- src/Rapido.Web.UI/Program.cs
+++ src/Rapido.Web.UI/Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 using Rapido.Web.Core;
 using Rapido.Web.UI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.Services.AddCore();
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000") });
 
