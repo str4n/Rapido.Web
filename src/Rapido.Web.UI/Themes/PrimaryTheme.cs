558997d93511d98d7e09a0175b848a11bc8fcca3
Kamil <Str4ngerGameOne@gmail.com>
2024-11-05 17:27:05 +0100

    MudBlazor theme

---
+++ src/Rapido.Web.UI/Themes/PrimaryTheme.cs
@@ -0,0 +1,17 @@
+﻿using MudBlazor;
+
+namespace Rapido.Web.UI.Themes;
+
+public sealed class PrimaryTheme : MudTheme
+{
+    public PrimaryTheme()
+    {
+        PaletteLight = new PaletteLight
+        {
+            Primary = "#000b4b",
+            Secondary = "#0070E0",
+            Tertiary = "#1e30f3",
+            Background = "#f5f5f5"
+        };
+    }
+}
